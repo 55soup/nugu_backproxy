11aafa16f77be2848d1d8a2c1b353688caa17c0d
Park Sunjoo <w2118@e-mirim.hs.kr>
2023-06-23 00:02:49 +0900

    Fix: to nugujs

--- server.js
+++ server.js
@@ -3,12 +3,25 @@ const nuguApp = require('nugujs');
 const app = express()
 let output = {}
 
-app.get('/textSay',  (req, res) => {
+app.get('/recommend',  (req, res) => {
   const nugu = nuguApp(req, res);
-  let value1 = "Day before yesterday I saw a rabbit, and yesterday a deer,"
-  let value2 = "and today, you"
-  output.value1 = value1
-  output.value2 = value2
+  const menu = nugu.get(inputMenu);
+
+  const rice = ['현미밥', '잡곡밥', '흑미밥', '찰현미밥', '보리밥'];
+  const bread = ['호밀빵', '식빵', '햄치즈 샌드위치'];
+  const salad = ['치킨텐더 샐러드', '아보카도 샐러드', '연어샐러드'];
+  const randomIndex = Math.floor(Math.random() * breakfastOptions.length);
+  let ment;
+  // let ment = "아침으로 "
+  
+  if(menu == '밥'){
+    ment = {"prompt" : rice[Math.floor(Math.random() * rice.length)] + "어떠세요?"}
+  }else if(menu == '빵'){
+    ment = {"prompt" : bread[Math.floor(Math.random() * bread.length)] + "어떠세요?"}
+  }else if(menu == "샐러드"){
+    ment = {"prompt" : salad[Math.floor(Math.random() * bread.length)] + "어떠세요?"}
+  }
+  output.recommMenu = ment;
   nugu.say(output);
 })
 
